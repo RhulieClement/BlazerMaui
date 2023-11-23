26fee52eda2fb5f4152525841a98075928fe9a0e
Rhulani Mathe <rhulani@esoftwaresolutions.co.za>
2023-11-23 15:49:22 +0200

    first push

---
+++ BlazerMaui/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazerMaui;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ RazorLibrary/Models/TodoModel.cs
@@ -0,0 +1,8 @@
+﻿
+namespace RazorLibrary.Models;
+
+public class TodoModel
+{
+    public string TodoItem { get; set; }
+    public int IsComplete { get; set; }
+}
